0ef98b2bd9a680134914f99f02cc27a74ec4c126
barepsetiyadi <barepset@gmail.com>
2022-06-08 19:12:08 +0700

    Disable clour choose

--- src/components/SwatchList/SwatchList.js
+++ src/components/SwatchList/SwatchList.js
@@ -6,21 +6,21 @@ import * as styles from './SwatchList.module.css';
 const SwatchList = (props) => {
   const { swatchList, activeSwatch, setActiveSwatch } = props;
   return (
-    <div className={styles.root}>
-      <span className={styles.label}>Select Colour: {activeSwatch.title}</span>
-      <div className={styles.swatchSelection}>
-        {swatchList?.map((colorChoice, index) => {
-          return (
-            <Swatch
-              key={index}
-              data={colorChoice}
-              setActiveSwatch={setActiveSwatch}
-              isActive={activeSwatch === colorChoice}
-            />
-          );
-        })}
-      </div>
-    </div>
+    // <div className={styles.root}>
+    //   <span className={styles.label}>Select Colour: {activeSwatch.title}</span>
+    //   <div className={styles.swatchSelection}>
+    //     {swatchList?.map((colorChoice, index) => {
+    //       return (
+    //         <Swatch
+    //           key={index}
+    //           data={colorChoice}
+    //           setActiveSwatch={setActiveSwatch}
+    //           isActive={activeSwatch === colorChoice}
+    //         />
+    //       );
+    //     })}
+    //   </div>
+    // </div>
   );
 };
 
