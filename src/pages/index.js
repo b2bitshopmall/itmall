4b69e9c69cc46d78d591b46376043b4ef1235089
barepsetiyadi <barepset@gmail.com>
2022-06-08 13:44:03 +0700

    Update PDP

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Special cable – Made in Germany'}
+        subtitle={'As a cable manufacturer, we are pioneers in communication technology.'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
